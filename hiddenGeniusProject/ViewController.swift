379d38f200c0826504e82b899f74817dd1ce1088
Galanafai <galanafai.windross@hgs.hiddengeniusproject.org>
2016-07-08 11:24:26 -0700

    Galanafai

--- hiddenGeniusProject/ViewController.swift
+++ hiddenGeniusProject/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var killer: UILabel!
+    @IBOutlet weak var keemstar: UIButton!
+    @IBOutlet weak var bg1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func kkkk(sender: AnyObject) {
+    killer.hidden = true
+    keemstar.hidden = false
+    bg1.hidden = false
+    
+    }
 }
 
